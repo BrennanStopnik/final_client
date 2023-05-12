afd5b0b9105b4afa41c52c4f559dbf65a0653a9d
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2023-05-12 16:48:03 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ const App = () => {
       children: [
         {
           path: "/home",
+          index: true,
           element: <HomePage urlEndPoint={urlEndPoint} />,
         },
         {
